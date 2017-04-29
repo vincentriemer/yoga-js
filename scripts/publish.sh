a89bd60423468377b919b72ba8bfa095c7221453
Vincent Riemer <vincentriemer@gmail.com>
2017-04-29 15:00:40 -0400

    ci($publish): fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-
+BRANCH="$TRAVIS_BRANCH"
+PULL_REQUEST="$TRAVIS_PULL_REQUEST"
 LATEST_BRANCH="master"
 NEXT_BRANCH="develop"
 
-if [ "$BRANCH" != "master" ] && [ "$BRANCH" != "develop" ];
+if [ "$BRANCH" != "$LATEST_BRANCH" ] && [ "$BRANCH" != "$NEXT_BRANCH" ];
 then echo "not a valid publishing branch, skipping..." && exit 1;
 fi
 
@@ -15,10 +16,10 @@ fi
 export PACKAGE_VERSION='cat package.json | jq .version -r'
 export PACKAGE_NAME='cat package.json | jq .name -r'
 
-if [ "NPM_PUBLISHED" == "1" && "$BRANCH" == "develop"]; then;
+if [ "NPM_PUBLISHED" == "1" ] && [ "$BRANCH" == "$LATEST_BRANCH"]; then;
   npm dist-tag add $PACKAGE_NAME@$PACKAGE_VERSION next;
 fi
 
-if [ "NPM_PUBLISHED" == "1" && "$BRANCH" == "master" ]; then;
+if [ "NPM_PUBLISHED" == "1" ] && [ "$BRANCH" == "$NEXT_BRANCH" ]; then;
   npm dist-tag add $PACKAGE_NAME@$PACKAGE_VERSION latest;
 fi
