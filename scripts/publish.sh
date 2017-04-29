ca0ab60257b3b44a24b033faa54ef2f14a7e4c2f
Vincent Riemer <vincentriemer@gmail.com>
2017-04-29 15:08:46 -0400

    ci($publish): fix syntax error in publish script (kill me)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,17 +8,17 @@ if [ "$BRANCH" != "$LATEST_BRANCH" ] && [ "$BRANCH" != "$NEXT_BRANCH" ];
 then echo "not a valid publishing branch, skipping..." && exit 1;
 fi
 
-if [ "$PULL_REQUEST" == "false"]; then;
+if [ "$PULL_REQUEST" == "false"]; then
   npm run semantic-release && set NPM_PUBLISHED=1 || echo "Unable to publish version" && set NPM_PUBLISHED=0;
 fi
 
 export PACKAGE_VERSION='cat package.json | jq .version -r'
 export PACKAGE_NAME='cat package.json | jq .name -r'
 
-if [ "NPM_PUBLISHED" == "1" ] && [ "$BRANCH" == "$LATEST_BRANCH"]; then;
+if [ "NPM_PUBLISHED" == "1" ] && [ "$BRANCH" == "$LATEST_BRANCH"]; then
   npm dist-tag add $PACKAGE_NAME@$PACKAGE_VERSION next;
 fi
 
-if [ "NPM_PUBLISHED" == "1" ] && [ "$BRANCH" == "$NEXT_BRANCH" ]; then;
+if [ "NPM_PUBLISHED" == "1" ] && [ "$BRANCH" == "$NEXT_BRANCH" ]; then
   npm dist-tag add $PACKAGE_NAME@$PACKAGE_VERSION latest;
 fi
